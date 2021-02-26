361b88b2a77246edf6268f03a7756ff2b42ced6e
lmOTW <35335152+louisleo234@users.noreply.github.com>
2021-02-26 11:11:50 +0800

    py_basic_homework_3(py正则表达式匹配交换机接口数据)

---
+++ py05_re.py
@@ -0,0 +1,11 @@
+import re
+
+str1 = 'Port-channel1.189       192.168.189.254     YES     CONFIG  up  up'
+
+result = re.match('(\w+-\w+\d+\.\d{1,3})\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(YES)\s+(CONFIG)\s+(\w+)\s+(\w+)', str1).groups()
+print(result)
+print('-'*64)
+print('{0:<8}:{1:>18}'.format('接口', result[0]))
+print('{0:<8}:{1:>16}'.format('IP地址', result[1]))
+print('{0:<8}:{1:>3}'.format('状态', result[4]))
+
