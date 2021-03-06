d04e0b2a47c921d29230aed913901e44259d6c4e
lmOTW <35335152+louisleo234@users.noreply.github.com>
2021-03-06 17:03:10 +0800

    py_basic_homework_01_20210306

---
+++ day01_20210306/Python_First.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.6
+# -*- coding=utf-8 -*-
+
+result = 1 + 2
+print('计算1 + 2的结果')
+print('计算结果为: ' + str(result))
---
+++ day01_20210306/Random_IPv4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.6
+# -*- coding=utf-8 -*-
+
+import random
+ip_oct1 = random.randint(1,255)
+ip_oct2 = random.randint(1,255)
+ip_oct3 = random.randint(1,255)
+ip_oct4 = random.randint(1,255)
+print(str(ip_oct1) + '.' + str(ip_oct2) + '.' + str(ip_oct3) + '.' + str(ip_oct4))
