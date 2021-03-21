0cf43129acfc5b6ad980035bdf51d317d622d0f1
lmOTW <35335152+louisleo234@users.noreply.github.com>
2021-03-21 23:31:04 +0800

    py_basic_homework_08_20210316

--- day08_20210316/kamene_ping_function.py
+++ day08_20210316/kamene_ping_function.py
@@ -9,23 +9,28 @@ logging.getLogger("kamene.runtime").setLevel(logging.ERROR)
 from kamene.all import *
 
 # kamene方法
-# def qytang_ping(host):
-#     ping_pkt = IP(dst=host)/ICMP()
-#     ping_result = sr1(ping_pkt, timeout=2,verbose=False)
-#     if ping_result == 0:
-#         print('IP可达')
-#         return ping_result
-#     else:
-#         print('IP不可达，5秒后重试')
+def qytang_ping(ip):
+    ping_pkt = IP(dst=ip) / ICMP()
+    ping_result = sr1(ping_pkt, timeout=2,verbose=False)
+    if ping_result:
+        return ip, True
+    else:
+        return ip, False
 
 # os ping函数方法
-import os
-def os_ping():
-    ping_test1 = os.system('ping 127.0.0.1 -c 4')
-    if ping_test1 == 0:
-        print('IP可达')
-    else:
-        print('IP不可达')
-    return ping_test1
+# import os
+# def os_ping():
+#     ping_test1 = os.system('ping 127.0.0.1 -c 1')
+#     if ping_test1 == 0:
+#         print('IP可达')
+#     else:
+#         print('IP不可达')
+#     return ping_test1
 
-os_ping()
+
+if __name__ == '__main__':
+    ping_pong = qytang_ping('172.16.240.20')
+    if ping_pong[1]:
+        print(f'{ping_pong[0]} 可达！')
+    else:
+        print(f'{ping_pong[0]} 不可达!')
